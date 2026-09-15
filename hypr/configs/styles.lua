2283ffabea08c3889d78e2071de3d28c80c47aba
Grant Oganyan <oganyan1500@gmail.com>
2026-09-15 16:30:15 -0400

    Commit

--- hypr/configs/styles.lua
+++ hypr/configs/styles.lua
@@ -2,7 +2,7 @@
 hl.exec_cmd('gsettings set org.gnome.desktop.interface gtk-theme "Fluent-round-grey-Dark"') -- for GTK3 apps
 hl.exec_cmd('gsettings set org.gnome.desktop.interface icon-theme "Fluent-dark"')
 hl.exec_cmd('gsettings set org.gnome.desktop.interface color-scheme "prefer-dark"') -- for GTK4 apps
-hl.exec_cmd("gsettings set org.gnome.desktop.interface text-scaling-factor 1") -- Set global scaling for text slightly smaller
+hl.exec_cmd("gsettings set org.gnome.desktop.interface text-scaling-factor 0.9") -- Set global scaling for text slightly smaller
 hl.env("QT_QPA_PLATFORMTHEME", "qt6ct") -- for Qt apps
 
 hl.config({
